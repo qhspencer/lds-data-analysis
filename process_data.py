83bf48c63c6f2398bb2a8fb81212ada353472ddd
Quentin Spencer <qspencer@ieee.org>
2018-10-13 21:00:48 -0500

    Move search data to json file

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@
 import pandas
 import os
 import glob
+import json
 import datetime
 
 import matplotlib
@@ -10,6 +11,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as pl
 pl.style.use('fivethirtyeight')
 import matplotlib.dates as mdates
+
+searches = json.load(open('search_data.json'))
 
 #dfs = [pandas.read_json(file) for file in os.listdir('data')]
 dfs = []
@@ -26,50 +29,93 @@ df_all['author'] = df_all['author'].str.replace('^(President|Elder|Bishop) ', ''
 df_all['author'] = df_all['author'].str.replace('Wm.', 'William')
 df_all = df_all.reset_index()
 
-searches = ({'file':'revelation',
-             'include':'revelation'},
-            {'file':'cp',
-             'include':'covenant path'},
-            {'file':'keys',
-             'include':'keys'},
-            {'file':'satan',
-             'include':'(satan|lucifer)'},
-            {'file':'savior',
-             'include':'the savior'},
-            {'file':'jesus',
-             'include':'jesus',
-             'exclude':'church of jesus christ'},
-            {'file':'mormon',
-             'include':'mormon',
-             'exclude':'book of mormon'},
-            {'file':'grace',
-             'include':'grace'},
-            {'file':'tm',
-             'include':'tender (mercy|mercies)'})
+# remove unneeded characters
+df_all['body'] = df_all['body'].str.replace('\t|\n', '')
+df_all['tail'] = df_all['body'].str[-31:].str.lower()
 
-for search in searches:
-    matches = df_all['body'].str.lower().str.count(search['include'])
-    if 'exclude' in search.keys():
-        matches -= df_all['body'].str.lower().str.count(search['exclude'])
+for search in [searches[2]]:
+#for search in searches:
+    results = pandas.DataFrame()
 
-    #results = df_all.assign(matches=matches)[matches>0][['date', 'author', 'matches']]
+    for s in search['search']:
+        matches = df_all['body'].str.lower().str.count(s['include'])
+        if 'exclude' in s.keys():
+            for excl_str in s['exclude']:
+                matches -= df_all['body'].str.lower().str.count(excl_str)
 
-    results = df_all.assign(matches=matches).groupby('date').sum()['matches']
+        results[s['label']] = df_all.assign(matches=matches).groupby('date').sum()['matches']
 
     fig, ax = pl.subplots(figsize=(12,5))
-    #results.plot.bar(ax=ax)
-
-    ax.bar(results.index, results, width=120)
     ax.xaxis.set_minor_locator(mdates.YearLocator(1, month=3))
     ax.xaxis.set_major_locator(mdates.YearLocator(5, month=3))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-    ax.set_xlim([datetime.date(1971, 1, 1),
-                 datetime.date(2019, 4, 1)])
+    results.plot.bar(ax=ax, width=2)
+
+    #ax.bar(results.index, results, width=120)
+    #ax.set_xlim([datetime.date(1971, 1, 1),
+    #             datetime.date(2019, 4, 1)])
     pl.ylabel('uses per conference')
-    pl.title('Uses of "{0:s}"'.format(search['include']))
+    #pl.title('Uses of "{0:s}"'.format(search['include']))
     pl.savefig('/mnt/d/Quentin/' + search['file'] + '.png')
 
 
+# Process references
 
+# retrieve from body text using regular expression and create new dataframe
 refs = df_all['body'].str.findall('\(.*?[0-9].*?\)')
-rdf = refs.apply(pandas.Series).stack().to_frame()
+rdf = refs.apply(pandas.Series).stack().to_frame('ref')
+ref_df = rdf.reset_index(0).join(df_all, on='level_0')[['date', 'author', 'ref']]
+
+# strip extra characters from references and delete things that don't look like references
+ref_df = ref_df[ref_df['ref'].str.contains(':')]
+ref_df['ref'] = ref_df['ref'].str.strip('( ).')
+
+# split multiple references
+needs_split = ref_df['ref'].str.contains(';')
+non_split = ref_df[~needs_split]
+to_split = ref_df[needs_split].reset_index()
+split_refs = to_split['ref'].str.split(';').apply(pandas.Series).stack().to_frame('ref')
+srdf = split_refs.reset_index(0).join(to_split[['date', 'author']], on='level_0')[['date', 'author', 'ref']]
+
+ref_df = pandas.concat([non_split, srdf[srdf['ref'].str.contains(':')]])
+ref_df['ref'] = ref_df['ref'].str.strip(' ')
+
+# replace strings with standardized versions
+replace = {'Doctrine and Covenants':'D&C',
+           u'Joseph Smith\u2014History':u'JS\u2014H',
+           'Joseph Smith Translation': 'JST',
+           'Articles of Faith':'A of F',
+           'Moroni':'Moro.',
+           'Helaman':'Hel.',
+           'Nephi':'Ne.',
+           'Genesis':'Gen.',
+           'Exodus':'Ex.',
+           'Leviticus':'Lev.',
+           'Numbers':'Num.',
+           'Deuteronomy':'Deut.',
+           'Isaiah':'Isa.',
+           'Jeremiah':'Jer.',
+           'Daniel':'Dan.',
+           'Malachi':'Mal.',
+           'Matthew':'Matt.',
+           'Romans':'Rom.',
+           'Corinthians':'Cor.',
+           'Ephesians':'Eph.',
+           'Galatians':'Gal.',
+           'Timothy':'Tim.',
+           'Hebrews':'Heb.',
+           'Peter':'Pet.',
+           'Revelation':'Rev.',
+           '^(S|s)ee ':'',
+           'see also ':'',
+           'from ':'',
+           ' [in the Bible appendix]':''}
+
+for k, v in replace.iteritems():
+    ref_df['ref'] = ref_df['ref'].str.replace(k, v)
+
+# get rid of short strings (probably footnotes) and long references
+ref_df = ref_df[(ref_df['ref'].str.len()>2) & (ref_df['ref'].str.len()<44)]
+
+ref_df['book'] = ref_df['ref'].str.replace(' [0-9]*\:[-0-9]*','')
+ref_df['len'] = ref_df['ref'].str.len()
