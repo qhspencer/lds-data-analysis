58579b1e7206d7b6217c5a7b0c0249ae4cbb9ea7
Quentin Spencer <qspencer@ieee.org>
2019-09-29 15:29:50 -0500

    New search and add command-line parsing to run_searches.py

--- run_searches.py
+++ run_searches.py
@@ -5,25 +5,38 @@ import os
 import glob
 import json
 import datetime
+import argparse
 from data_utils import *
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as pl
-#pl.style.use('fivethirtyeight')
-pl.style.use('bmh')
 import matplotlib.dates as mdates
-pl.rcParams['figure.max_open_warning'] = 50
 
-search_file = 'search_data.json'
-output_dir = '/mnt/d/Quentin/'
-time_axis = 'conf'
+parser = argparse.ArgumentParser()
+parser.add_argument('--mpl-style', dest='mpl_style', type=str, default='bmh',
+                    choices=['bmh', 'fivethirtyeight'], help='style package for matplotlib plots')
+parser.add_argument('--output-dir', dest='output_dir', type=str, default='.',
+                    help='path for saving output files')
+parser.add_argument('--search-data', dest='search_data', type=str, default='search_data.json',
+                    help='JSON file containing search data')
+parser.add_argument('--time-axis', dest='time_axis', type=str, default='year',
+                    choices=['year', 'conf'], help='specifies spacing of time axis')
+args = parser.parse_args()
+
+# This needs to be set high if a large number of plots are being generated
+pl.rcParams['figure.max_open_warning'] = 50
+pl.style.use(args.mpl_style)
+
+search_file = args.search_data
+output_dir = args.output_dir
+time_axis = 'year'
 
 print "Loading data"
 searches = json.load(open(search_file))
 
 first_year = 1971
-last_year = datetime.date.today().year #+ 1
+last_year = datetime.date.today().year + 1
 if time_axis == 'year':
     group = 'year'
     daterange = [first_year, last_year]
@@ -38,10 +51,7 @@ elif time_axis == 'conf':
 df_all = load_data()
 talks_only = get_only_talks(df_all)
 talk_counts = talks_only.groupby('date').count()['year'].to_frame('talks')
-#df_all['tail'] = df_all['body'].str[-31:].str.lower()
 
-
-#for search in [searches[2]]:
 for search in searches:
     print 'running search:', search['search']
     if 'case sensitive' in search.keys() and search['case sensitive']=='true':
@@ -70,11 +80,6 @@ for search in searches:
 
     fig, ax = pl.subplots(figsize=(12,5))
     results.plot(ax=ax)
-    #ax.bar(results.index, results, width=120)
-
-    #ax.xaxis.set_minor_locator(mdates.YearLocator(1, month=3))
-    #ax.xaxis.set_major_locator(mdates.YearLocator(5, month=3))
-    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 
     ax.set_xlim(daterange)
     pl.ylabel(yaxis_str)
