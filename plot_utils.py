ddd0ec12bd21e1ed9d71708906abce0e45fea59b
Quentin Spencer <qspencer@ieee.org>
2021-06-03 01:29:58 +0100

    Update presidents notebook

--- plot_utils.py
+++ plot_utils.py
@@ -3,9 +3,9 @@ from data_utils import *
 
 apostle_data = load_apostle_data()
 
-def add_president_terms():
+def add_president_terms(color='#cfcfcf'):
     presidents = apostle_data[~apostle_data['sdate_p'].isna()]
     pres_terms = presidents[['name', 'sdate_p', 'dod']].set_index('name')
     for pr in ['George Albert Smith', 'Joseph Fielding Smith', 'Spencer W. Kimball',
                'Howard W. Hunter', 'Thomas S. Monson']:
-        pl.axvspan(pres_terms.loc[pr, 'sdate_p'], pres_terms.loc[pr, 'dod'], color='#dddddd', alpha=1, lw=2)
+        pl.axvspan(pres_terms.loc[pr, 'sdate_p'], pres_terms.loc[pr, 'dod'], color=color, alpha=1, lw=0.5, ec='k')
